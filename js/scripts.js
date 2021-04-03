1fddc8b0a2e91bc3266e737f081c048dfeaae2e8
Ellen Studer <ellenmstuder@gmail.com>
2021-04-02 21:57:31 -0500

    removed test-code; established 'pokemonList' in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-let favFood = 'gyros';
-document.write(favFood);  
+// establish 'pokemonList'-- will contain pokemon data
+let pokemonList = [];
+
+// add pokemon data
+pokemonList[0] = {name: "onix", height: 15, types: ["rock", "ground"]}
+pokemonList[1] = {name: "ratatat", height: 1, types: ["pest", "normal"]}
+pokemonList[2] = {name: "pidgeotto", height: 2, types: ["bird", "normal"]}

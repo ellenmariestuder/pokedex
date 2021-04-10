4506e7c5ff8a06a0013bdea8852d628f95ac8ca6
Ellen Studer <ellenmstuder@gmail.com>
2021-04-10 18:48:49 -0500

    implementing IIFE to hold data and functions; updating forEach accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,44 @@
 
-// establish 'pokemonList'
-let pokemonList = [
+// create pokemonRepository to hold pokemonList + IIFE
+let pokemonRepository = (function() {
 
-  // add pokemon data
-  {
-    name: "onix",
-    height: 15,
-    types: ["rock", "ground"]
-  },
-  {
-    name: "ratatat",
-    height: 1,
-    types: ["pest", "normal"]
-  },
-  {
-    name: "pidgeotto",
-    height: 2,
-    types: ["bird", "normal"]
+  // establish 'pokemonList'
+  let pokemonList = [
+    // add pokemon data
+    {
+      name: "onix",
+      height: 15,
+      types: ["rock", "ground"]
+    },
+    {
+      name: "ratatat",
+      height: 1,
+      types: ["pest", "normal"]
+    },
+    {
+      name: "pidgeotto",
+      height: 2,
+      types: ["bird", "normal"]
+    }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+
+}) ();
 
 // use forEach to write pokemon data to DOM
-pokemonList.forEach((item) => {
+pokemonRepository.getAll().forEach((item) => {
   // establish data we want to write document
   let pokeNameHeight = item.name + " (height: " + item.height + ")";
 
